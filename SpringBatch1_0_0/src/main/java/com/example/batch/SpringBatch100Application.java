fc1a3ca5dc739b5c01ea378c9c4e1b113ed0302f
Aman Jogi <aman.jogi.j@gmail.com>
2023-06-27 21:14:45 +0530

    initial commit spring batch

---
+++ SpringBatch1_0_0/src/main/java/com/example/batch/SpringBatch100Application.java
@@ -0,0 +1,13 @@
+package com.example.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch100Application.class, args);
+	}
+
+}
